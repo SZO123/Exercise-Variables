3a21c11dd826c25ca7ff024fd6662f7e702b44c9
Najib <najib200983@outlook.com>
2025-09-24 13:25:33 -0700

    .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
+    public static void main(String[] args)
+    {
 
     int vehicleID;
     int i = 200;
@@ -23,5 +24,27 @@ void main() {
     System.out.println("Full bags;" + FullBags);
     System.out.println("Left over;" + leftover);
 
-
+    {
+    double number = 4.3;
+    double number1 = number * 2;
+    System.out.println("original: " + number);
+    System.out.println("Doubled: " + number1);
 }
+    {
+        String name = "Najib";
+        int FavNumber = 20;
+        System.out.println("My name is " + name + " and my favorite number is " + FavNumber);
+
+    }
+
+    {
+    int randomNumber = (int)(Math.random() * 11);
+        System.out.println("Random Number: " + randomNumber);
+        int NewValue = randomNumber + 5;
+        System.out.println("New value: " + NewValue);
+
+
+    }
+}
+
+
