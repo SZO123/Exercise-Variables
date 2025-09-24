aa043cbd6eff488334237445234b13e59452914f
Najib <najib200983@outlook.com>
2025-09-24 10:19:48 -0700

    .

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+    int vehicleID;
+    int i = 200;
+    String VehicleMakeModel;
+    String VehicleColer;
+    Boolean HasTowingPackage;
+    int Odometer;
+    double price;
+    char qualityrating;
+    String PhoneNumber;
+    String SSN;
+    String ZipCode;
+
+    int totalCoins = 137;
+    int CoinsPerBag = 20;
+
+    int FullBags = totalCoins / CoinsPerBag;
+    int leftover = totalCoins % CoinsPerBag;
+
+    System.out.println("Full bags;" + FullBags);
+    System.out.println("Left over;" + leftover);
+
+
+}
